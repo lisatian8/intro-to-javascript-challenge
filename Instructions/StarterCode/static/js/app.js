37dc23c096d30c86979b2d6eed7832fcc0923286
lisatian8 <lt8760@yahoo.com>
2019-08-08 10:22:46 -0400

    Instructions/

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -89,111 +89,70 @@ allData.forEach(item => {
 
 console.log("here 3");
 
+//test
 
+var lists=[["A", 1], ["B, 2"], ["c", 3]];
 
+console.log(lists);
+console.log(lists.filter(list => list[0] === "A"));
+var lists2 = lists.filter(list => list[0] === "A");
+console.log("lists2 : " + lists2);
 
 console.log("here 3b");
 
-//below are tests
+// end of test
 
-var cityMapped = newTableData.map(newTableDataRow => newTableDataRow.city);
-var datetimeMapped = newTableData.map(newTableDataRow => newTableDataRow.datetime);
-
-console.log(cityMapped);
-console.log(datetimeMapped);
-
-console.log("here 4");
-// Create a custom filtering function
-function selectCity(observation) {
-  return observation.city === 'benton';
-}
-
-// filter() uses the custom function as its argument
-var cityNewTableData= newTableData.filter(selectCity);
-
-// Test
-console.log(cityNewTableData);
-
-
-console.log("here 5");
-function selectCity2(observation) {
-  return observation.city === 'bonita';
-}
-
-// filter() uses the custom function as its argument
-var cityNewTableData2= newTableData.filter(selectCity2);
-
-console.log(cityNewTableData2);
-console.log("here 6");
-
-// filter() uses the custom function as its argument
-function selectDatatime(observation) {
-  return observation.datetime === '1/1/2010';
-}
-
-var cityNewTableData3= newTableData.filter(selectDatatime);
-console.log(cityNewTableData3);
-
-console.log("here 7");
-
-
-
-
-//test
-var testArr = [];
-//var testArr[0] = [];
-var student1 = ["Mike", "99"];
-var  student2= ["Peter", "98"];
-
-testArr.push(student1);
-//testArr[0].push(grades[0]);
-testArr.push(student2);
-console.log(testArr);
-//console.log(testArr[0]);
-//console.log(students);
-//console.log(grades);
-
-
-
-// test
-console.log("here 9");
-// 
-
-// tests
+console.log("here 3c");
 
 var ufos = data;
 
-
+// Select the button
 var button = d3.select("#filter-btn");
-var tbody = d3.select("tbody");
+var tbody2 = d3.select("tbody");
+var choiceNames = ["#datetime", "#city", "#state", "#country", "#shape"]
 
 button.on("click", function() {
 
+  // Select the input element and get the raw HTML node
+
   var inputElement = d3.select("#datetime");
+  //var inputElement = d3.select("#city");
+
+  // Get the value property of the input element
+
   var inputValue = inputElement.property("value");
+
   console.log(inputValue);
   console.log(ufos);
+
   var filteredData = ufos.filter(ufo => ufo.datetime === inputValue);
+ //var filteredData = ufos.filter(ufo => ufo.city === inputValue);
+
   console.log(filteredData);
 
-  // Iterate through each student/grade pair
+  var tbody = d3.select("tbody");
   filteredData.forEach(item => {
+    console.log("here 21");
+    console.log("item: " + item);
+    console.log("object keys: " + Object.keys(item));
+    console.log("object values: " + Object.values(item));
+    console.log("object entries: " + Object.entries(item));
+    console.log("here 22");
+    var row = tbody.append("tr");
   
-  console.log(item);
-  var row = tbody.append("tr");
 
-  // append one cell for the student and one cell for the grade
-  row.append("td").text(item[0]);
-  row.append("td").text(item[1]);
-  row.append("td").text(item[2]);
-  row.append("td").text(item[3]);
-  row.append("td").text(item[4]);
-  row.append("td").text(item[5]);
-  row.append("td").text(item[6]);
-  
-});
-  //console.log("Hi, a button was clicked!");
- // console.log(d3.event.target);
-});
-console.log("here 10");
 
+    filteredData.forEach((item) => {
+      Object.entries(item).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      //  console.log("key : " + key);
+      //  console.log("value: " + value);
+      });
+    });
+    
+  });
+
+});
+
+console.log("here 20");
